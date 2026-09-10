8e77e040400b2ac881f5e461b7864937e8759aed
Windscribe Fork Contributor <dev@windscribe-fork.local>
2026-09-10 06:21:58 +0330

    feat(build): enable ABI splits for 4 Android architectures and universal APK

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -25,6 +25,14 @@ android {
             enableSplit = false
         }
     }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
     buildFeatures {
         buildConfig = true
         viewBinding = true
